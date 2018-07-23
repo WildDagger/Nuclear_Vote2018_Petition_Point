8feec7365ca0ec0307522cf8f1996e63291b4953
WildDagger Ho <hoyunxian@gmail.com>
2018-07-23 12:06:01 +0800

    程式初始化

---
+++ js/location.js
@@ -0,0 +1,122 @@
+var petition_location = [
+  {
+    id: 'taipei_001',
+    city: '台北市',
+    title: '台北市議員王鴻薇辦公室',
+    address: '台北市延壽街145號2樓',
+    tel: '02-27563646',
+    openTime: '',
+    comment: '',
+    position: { lat: 25.0569558, lng: 121.5632166 }
+  },
+  {
+    id: 'taipei_002',
+    city: '台北市',
+    title: 'MaSe咖啡',
+    address: '台北市八德路二段300巷38號',
+    tel: '02-27771231',
+    openTime:  '',
+    comment: '',
+    position: { lat: 25.0461232, lng: 121.5423724 }
+  },
+  {
+    id: 'taipei_003',
+    city: '台北市',
+    title: '台北市議員候選人張榮法服務處',
+    address: '台北市萬華區萬大路63號1樓',
+    tel: '02-23023625',
+    openTime: '',
+    comment: '',
+    position: { lat: 25.0308775, lng: 121.5007555 }
+  },
+  {
+    id: 'new_taipei_001',
+    city: '新北市',
+    title: '新北市議員候選人唐慧琳辦公室',
+    address: '新北市新店區北新路一段201號2樓',
+    tel: '02-29175777',
+    openTime: '',
+    comment: '',
+    position: { lat: 24.965012, lng: 121.540189 }
+  },
+  {
+    id: 'taoyuan_001',
+    city: '桃園市',
+    title: '桃園市議員參選人牛煦庭辦公室',
+    address: '桃園市龜山區復興一路356之1號',
+    tel: '',
+    openTime: '',
+    comment: '',
+    position: { lat: 25.0556854, lng: 121.3622269 }
+  },
+  {
+    id: 'hsinchu_city_001',
+    city: '新竹市',
+    title: '新竹市長參選人許明財競選總部',
+    address: '新竹市中正路109號',
+    tel: '',
+    openTime: '',
+    comment: '',
+    position: { lat: 24.8073791, lng: 120.967753 }
+  },
+  {
+    id: 'taichung_001',
+    city: '台中市',
+    title: '台中市清水區南社里里長賴朝維服務處',
+    address: '台中市清水區南社里民族路二段75巷3號',
+    tel: '',
+    openTime: '',
+    comment: '',
+    position: { lat: 24.2645059, lng: 120.5603737 }
+  },
+  {
+    id: 'changhua_001',
+    city: '彰化縣',
+    title: '右舍咖啡',
+    address: '彰化縣員林市萬年路三段67號',
+    tel: '04-8364864',
+    openTime: '09:00-18:00',
+    comment: '',
+    position: { lat: 23.9575618, lng: 120.573998 }
+  },
+  {
+    id: 'yunlin_001',
+    city: '雲林縣',
+    title: '北港武德宮',
+    address: '雲林縣北港鎮華勝路330號',
+    tel: '05-7821445',
+    openTime: '08:00-20:00',
+    comment: '',
+    position: { lat: 23.5810346, lng: 120.2991545 }
+  },
+  {
+    id: 'tainan_001',
+    city: '台南市',
+    title: '台南市議員王家貞服務處',
+    address: '台南市東區崇善路192號1樓',
+    tel: '06-2906669',
+    openTime: '',
+    comment: '',
+    position: { lat: 22.9787599, lng: 120.2254638 }
+  },
+  {
+    id: 'taitung_001',
+    city: '台東縣',
+    title: '精工眼鏡行',
+    address: '台東縣台東市更生北路110號',
+    tel: '089-223320',
+    openTime: '星期一-六 09:00-21:00',
+    comment: '',
+    position: { lat: 22.7785562, lng: 121.1253842 }
+  },
+  {
+    id: 'hualien_001',
+    city: '花蓮縣',
+    title: '花蓮縣議員候選人魏嘉彥辦公室',
+    address: '花蓮縣花蓮市富強路20號',
+    tel: '089-223320',
+    openTime: '09:00-18:00',
+    comment: '',
+    position: { lat: 23.9952489, lng: 121.5961416 }
+  }
+]
---
+++ js/main.js
@@ -0,0 +1,131 @@
+var boolMapInit = false
+var currentPosition, markerDefault
+
+function initMap() {
+  boolMapInit = true
+
+  getCurrentPosition().then(function (defaultPosition) {
+    currentPosition = defaultPosition
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: defaultPosition,
+      zoom: 15
+    })
+
+    markerDefault = new google.maps.Marker({
+      map: map,
+      position: defaultPosition,
+      title: '您的位置'
+    })
+
+    if (jQuery) {
+      jQueryMain(currentPosition, map, jQuery)
+    }
+  })
+}
+
+function getCurrentPosition() {
+  return new Promise(function (resolve, reject) {
+    getCurrentPositionPromise(resolve, reject)
+  })
+}
+
+function getCurrentPositionPromise(resolve, reject) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+      lat: parseFloat(position.coords.latitude),
+      lng: parseFloat(position.coords.longitude)
+    }
+
+    console.log(pos)
+    resolve(pos)
+  }, function (err) {
+    var errMsg, pos, $alert, $errMsg
+    if (err.code == err.TIMEOUT) {
+      errMsg = '發生逾時，將採用預設位置'
+    } else if (err.code == err.PERMISSION_DENIED) {
+      errMsg = '未取得授權，將採用預設位置'
+    } else if (err.code == err.POSITION_UNAVAILABLE) {
+      errMsg = '無法取得位置，將採用預設位置'
+    } else {
+      errMsg = err.message
+    }
+
+    if (jQuery) {
+      (function($) {
+        $errMsg = $('#errMsg')
+        $alert = $('#alert')
+      })(jQuery)
+    }
+
+    // 顯示錯誤通知
+    if ($errMsg) {
+      $errMsg.text(errMsg)
+    } else {
+      console.log(errMsg)
+    }
+
+    if ($alert) {
+      $alert.alert()
+    }
+
+    // 將pos設定為預設位置 (台北車站)
+    pos = { lat: 25.048734, lng: 121.514231 }
+    resolve (pos)
+  }, {
+    enableHighAccuracy: true,
+    maximumAge: 0
+  })
+}
+
+function jQueryMain(currentPosition, map, $) {
+  var $pointTitle = $('#pointTitle')
+  var $pointAddress = $('#pointAddress')
+  var $pointTel = $('#pointTel')
+  var $pointOpenTime = $('#pointOpenTime')
+  var $btnGetPointRoute = $('#getPointRoute')
+  var $btnGetNearestPoint = $('#getNearestPoint')
+  var markerPoint = []
+
+  $.each(petition_location, function(index, item) {
+    var marker = new google.maps.Marker({
+      map: map,
+      position: item.position,
+      title: item.title,
+      icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+    })
+
+    marker.addListener('click', function() {
+      $pointTitle.text(item.title)
+      $pointAddress.text(item.address)
+      $pointTel.html('<a href="tel:' + item.tel + '">' + item.tel + "</a>")
+      $pointOpenTime.text(item.openTime)
+      $btnGetPointRoute.attr('href', generateDirectionUrl(currentPosition, item.position)).removeClass('disabled')
+
+      map.setZoom(15)
+      map.setCenter(marker.getPosition())
+    })
+
+    markerPoint.push(marker)
+  })
+
+  // console.log(markerPoint)
+
+  $btnGetNearestPoint.on('click', function(e) {
+    e.preventDefault()
+    alert('本功能尚未實裝')
+  })
+
+  function searchNearestPoint(currentPosition) {
+    //
+  }
+
+  function generateDirectionUrl(oriPos, desPos) {
+    var urlDirection = 'https://www.google.com/maps/dir/?api=1&origin={ori_lat},{ori_lng}&destination={des_lat},{des_lng}'
+
+    return urlDirection.replace('{ori_lat}', oriPos.lat)
+                       .replace('{ori_lng}', oriPos.lng)
+                       .replace('{des_lat}', desPos.lat)
+                       .replace('{des_lng}', desPos.lng)
+  }
+}
